c587cb083742df604bbae96a2cc782ca33fcbb51
noelfoka <nomgnenoel@gmail.com>
2023-09-22 01:41:11 +0100

    create table vets

--- schema.sql
+++ schema.sql
@@ -30,3 +30,10 @@ ADD COLUMN owner_id INTEGER;
 ALTER TABLE animals
  ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id),
  ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(150),
+	age INTEGER,
+	date_of_graduation DATE
+);
